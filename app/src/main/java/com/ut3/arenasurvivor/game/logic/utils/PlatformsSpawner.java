50b0faad79c71b10e184f54e236d84f102b6fb82
Samy206 <Samy.boumali@hotmail.com>
2023-03-13 17:40:22 +0100

    feature/Platform : added comment

--- app/src/main/java/com/ut3/arenasurvivor/game/logic/utils/PlatformsSpawner.java
+++ app/src/main/java/com/ut3/arenasurvivor/game/logic/utils/PlatformsSpawner.java
@@ -8,7 +8,6 @@ import com.ut3.arenasurvivor.entities.impl.Platform;
 public class PlatformsSpawner {
 
     private int windowWidth;
-    private int windowHeight;
 
     private final int PLATFORM_MAX_WIDTH  = 150;
     private final int PLATFORM_MIN_WIDTH = 70;
@@ -22,17 +21,16 @@ public class PlatformsSpawner {
 
     public PlatformsSpawner(int width, int height) {
         this.windowWidth = width;
-        this.windowHeight = height;
 
-
-        // Calculate the good height area to apply
+        // Setup limited height level to platforms spawn
         platformMaxHeight = height * 0.45;
         platformMinHeight = height * 0.55;
-
-
     }
 
-
+    /**
+     * Create a platform in a random position
+     * @return a created platform
+     */
     public Platform createPlatformWithRandomPos() {
 
         // Generate random platform width
@@ -44,7 +42,7 @@ public class PlatformsSpawner {
         int x = (int) (Math.random() * (windowWidth - width));
         int y = (int) (Math.random() * (platformMaxHeight - platformMinHeight) + platformMinHeight);
 
-        // Create rect
+        // Create rect with calculated coordinates
         Rect coordinates = new Rect(x, y, x + width, y + PLATFORM_HEIGHT);
 
         return new Platform(coordinates);
