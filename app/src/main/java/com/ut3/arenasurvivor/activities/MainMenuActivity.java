222d27b43cb2bf772ffd093ec85e638ac9bd7cd5
Eslam Humaid <eslam.humaid11@gmail.com>
2023-03-16 15:03:42 +0100

    Fixed persmessions request

--- app/src/main/java/com/ut3/arenasurvivor/activities/MainMenuActivity.java
+++ app/src/main/java/com/ut3/arenasurvivor/activities/MainMenuActivity.java
@@ -1,19 +1,30 @@
 package com.ut3.arenasurvivor.activities;
 
+import static androidx.constraintlayout.widget.StateSet.TAG;
+
+import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.Settings;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import com.ut3.arenasurvivor.R;
 
+
+
 public class MainMenuActivity extends AppCompatActivity {
     public static final String SHARED_PREF = "SCORE";
+    public static final int RECORD_AUDIO = 0;
     public Long scoreMax;
     private TextView scoreView;
 
@@ -33,6 +44,18 @@ public class MainMenuActivity extends AppCompatActivity {
         // Setting score value using shared preference
         scoreView = findViewById(R.id.Score);
         scoreView.setText("Meilleur score : " + scoreMax);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+            Log.d(TAG, "onResume: persmission granted== requested");
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO}, RECORD_AUDIO);
+
+        }
+
+        if (!Environment.isExternalStorageManager()){
+            Intent getPermission = new Intent();
+            getPermission.setAction(Settings.ACTION_MANAGE_ALL_FILES_ACCESS_PERMISSION);
+            startActivity(getPermission);
+        }
 
     }
 
